60c59a0a3483352a1654a47be974c8644555ed37
Advocad <advocad15@gmail.com>
2019-07-22 10:16:29 +0500

    fix php requirements

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,9 +1,12 @@
 <?php
 namespace app\controllers;
+
 use yii\web\Controller;
 
-class SiteController extends Controller {
-    public function actionIndex() {
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
         return  $this->render('index');
     }
 }
--- web/index.php
+++ web/index.php
@@ -2,15 +2,16 @@
 
 use yii\base\InvalidConfigException;
 
-define ('YII_DEBUG', true);
+define('YII_DEBUG', true);
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__ . '/../config/web.php';
 
 try {
     $yii = new yii\web\Application($config);
-} catch (InvalidConfigException $e) {
+} catch(InvalidConfigException $e) {
     $e->getTraceAsString();
 }
 $yii->run();
+
 
